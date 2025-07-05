4b4089b836163c02b18b8553eabd66fbdbf0240c
Prasanna Achar <prasannaachar64@gmail.com>
2025-07-05 22:17:46 +0530

    appjs

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })),
+app.use(express.urlencoded({ extended: true }));
 
 app.use(fileUpload({
     useTempFiles: true,
